0723632201f1707a8d8f89098f0824174d4644d1
odwyem14 <odwyem14@tcd.ie>
2026-02-21 16:09:21 +0000

    began work for demo business webpage

--- frontend/src/demo-site/DemoApp.jsx
+++ frontend/src/demo-site/DemoApp.jsx
@@ -1,7 +1,14 @@
+import React, {useEffect, useRef, useState} from "react";
+
 const DemoApp = () => {
     return (
         <div>
-            <h1>Demo App</h1>
+            <h1>Hack Attack Business</h1>
+            <h2>Curated for all your business needs</h2>
+
+            <div className = "chatbox">
+
+            </div>
         </div>
     )
 }
